c74193676399e3ee57e6c5ac42aae6a80049cf06
Sam Shareski <sam_shareski@otpp.com>
2018-02-07 17:07:35 -0500

    Add PropTypes to Form

--- src/console/Form.js
+++ src/console/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Form extends Component {
   constructor(props) {
@@ -46,6 +47,10 @@ class Form extends Component {
       </form>
     )
   }
+}
+
+Form.propTypes = {
+  submitFormData: PropTypes.func.isRequired
 }
 
 export default Form
