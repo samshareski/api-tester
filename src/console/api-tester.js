cd91407daa484b37c28b6b9a2b95548ffbc08b4b
Sam Shareski <sam_shareski@otpp.com>
2018-02-08 13:37:15 -0500

    Add conditional request body field

--- src/console/api-tester.js
+++ src/console/api-tester.js
@@ -1,9 +1,18 @@
-const makeApiRequest = async (method, url) => {
-  const init = {
-    method: method
+const makeApiRequest = async formData => {
+  let init = {
+    method: formData.method
+  }
+  if (formData.method === 'POST' || formData.method === 'PUT') {
+    init = {
+      ...init,
+      body: formData.body,
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }
   }
 
-  const response = await fetch(url, init)
+  const response = await fetch(formData.url, init)
   const json = await response.json()
 
   return {
