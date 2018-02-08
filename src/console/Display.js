7e4f2a486ce5d743e0bfb217e3e1d3d11bba5432
Sam Shareski <sam_shareski@otpp.com>
2018-02-08 11:20:19 -0500

    Return result object from API without async/await

--- src/console/Display.js
+++ src/console/Display.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
-const Display = ({ response }) => (
-  <pre>{response !== null ? response : 'Press TEST to test API'}</pre>
-)
+const Display = ({ response }) => {
+  if (response === null) {
+    return <p>Press TEST to test API</p>
+  } else {
+    return (
+      <div>
+        <p>Response is {response.ok ? 'OK' : 'not OK'}</p>
+        <pre>{response.json}</pre>
+      </div>
+    )
+  }
+}
 
 export default Display
--- src/console/api-tester.js
+++ src/console/api-tester.js
@@ -2,9 +2,17 @@ const makeApiRequest = (method, url) => {
   const init = {
     method: method
   }
+  const result = {}
+
   return fetch(url, init)
-    .then(response => response.json())
-    .then(json => JSON.stringify(json, null, 2))
+    .then(response => {
+      result.ok = response.ok
+      return response.json()
+    })
+    .then(json => {
+      result.json = JSON.stringify(json, null, 2)
+      return result
+    })
 }
 
 export default makeApiRequest
