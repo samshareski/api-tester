d353d294830c15eb84ede174d24434d252b32db6
Sam Shareski <sam_shareski@otpp.com>
2018-02-08 15:17:39 -0500

    Add validation on submit

--- src/console/Form.js
+++ src/console/Form.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+const URL_REGEX = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/
+
 class Form extends Component {
   constructor(props) {
     super(props)
     this.state = {
       method: 'GET',
       url: '',
-      body: ''
+      body: '',
+      submitted: false
     }
   }
 
@@ -24,13 +27,61 @@ class Form extends Component {
   }
 
   handleSubmit = event => {
-    this.props.submitFormData(this.state)
+    this.setState({
+      submitted: true
+    })
+
+    const errors = this.validate(
+      this.state.method,
+      this.state.url,
+      this.state.body
+    )
+
+    if (errors.length === 0) {
+      this.props.submitFormData({
+        method: this.state.method,
+        url: this.state.url,
+        body: this.state.body
+      })
+
+      // After successful submission reset form state
+      this.setState({
+        submitted: false
+      })
+    }
+
     event.preventDefault()
   }
 
+  validate = (method, url, body) => {
+    const errors = []
+
+    if (!URL_REGEX.test(url)) {
+      errors.push('Invalid URL')
+    }
+
+    if (method === 'POST' || method === 'PUT') {
+      try {
+        JSON.parse(body)
+      } catch (e) {
+        errors.push('Invalid JSON')
+      }
+    }
+
+    return errors
+  }
+
   render() {
+    let hasErrors = false
+    let errors = []
+    if (this.state.submitted) {
+      errors = this.validate(this.state.method, this.state.url, this.state.body)
+      hasErrors = errors.length > 0
+    }
+
     return (
       <form onSubmit={this.handleSubmit}>
+        {hasErrors && errors.map((error, index) => <p key={index}>{error}</p>)}
         <select
           name="method"
           value={this.state.method}
