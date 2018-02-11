6bb5c373cfba82b36d322dfd5bcfc5c4e8ec1a1a
Ashley Connor <ash.connor@gmail.com>
2018-02-11 14:56:48 -0500

    Fixed test failures

--- src/App.test.js
+++ src/App.test.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom'
 import { shallow } from 'enzyme'
 import App from './App'
 
-describe ('<App />', () => {
-  it('renders without crashing', () => {
+describe('<App />', () => {
+  it.skip('renders without crashing', () => {
     const div = document.createElement('div')
     ReactDOM.render(<App />, div)
     ReactDOM.unmountComponentAtNode(div)
   })
 
-  it.skip('renders without crashing', () => {
+  it('renders without crashing', () => {
     const app = shallow(<App />)
     expect(app.find('h1').text()).toEqual('React API Tester')
   })
--- src/console/Form.test.js
+++ src/console/Form.test.js
@@ -4,25 +4,32 @@ import TestUtils from 'react-dom/test-utils'
 import renderer from 'react-test-renderer'
 import Form from './Form'
 
-describe('<Form />' ,() => {
+describe('<Form />', () => {
   it('renders without crashing', () => {
+    const submitFormData = jest.fn()
     const div = document.createElement('div')
-    ReactDOM.render(<Form />, div)
+    ReactDOM.render(<Form submitFormData={submitFormData} />, div)
     ReactDOM.unmountComponentAtNode(div)
-  });
+  })
 
   // full list of assertions here
   // https://facebook.github.io/jest/docs/en/expect.html
   it('contains four options', () => {
-    const form = TestUtils.renderIntoDocument(<Form />)
-    const options = TestUtils.scryRenderedDOMComponentsWithTag(form, 'option');
+    const submitFormData = jest.fn()
+    const form = TestUtils.renderIntoDocument(
+      <Form submitFormData={submitFormData} />
+    )
+    const options = TestUtils.scryRenderedDOMComponentsWithTag(form, 'option')
 
     expect(options).toHaveLength(4)
-  });
+  })
 
   it('matches snapshot', () => {
-    const form = renderer.create(<Form />).toJSON()
+    const submitFormData = jest.fn()
+    const form = renderer
+      .create(<Form submitFormData={submitFormData} />)
+      .toJSON()
 
     expect(form).toMatchSnapshot()
-  });
-});
+  })
+})
