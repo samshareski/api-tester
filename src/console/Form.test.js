fa9503e85fbcb6256c315ddbeb0ab7789ca890fe
Ashley Connor <ash.connor@gmail.com>
2018-02-11 15:59:03 -0500

    Added test that simulates form submission

--- src/console/Form.test.js
+++ src/console/Form.test.js
@@ -4,25 +4,63 @@ import TestUtils from 'react-dom/test-utils'
 import renderer from 'react-test-renderer'
 import Form from './Form'
 
-describe('<Form />' ,() => {
+describe('<Form />', () => {
   it('renders without crashing', () => {
+    const submitFormData = jest.fn()
     const div = document.createElement('div')
-    ReactDOM.render(<Form />, div)
+    ReactDOM.render(<Form submitFormData={submitFormData} />, div)
     ReactDOM.unmountComponentAtNode(div)
-  });
+  })
 
   // full list of assertions here
   // https://facebook.github.io/jest/docs/en/expect.html
   it('contains four options', () => {
-    const form = TestUtils.renderIntoDocument(<Form />)
-    const options = TestUtils.scryRenderedDOMComponentsWithTag(form, 'option');
+    const submitFormData = jest.fn()
+    const form = TestUtils.renderIntoDocument(
+      <Form submitFormData={submitFormData} />
+    )
+    const options = TestUtils.scryRenderedDOMComponentsWithTag(form, 'option')
 
     expect(options).toHaveLength(4)
-  });
+  })
 
-  it('matches snapshot', () => {
-    const form = renderer.create(<Form />).toJSON()
+  it('matches default snapshot', () => {
+    const submitFormData = jest.fn()
+    const form = renderer
+      .create(<Form submitFormData={submitFormData} />)
+      .toJSON()
 
     expect(form).toMatchSnapshot()
-  });
-});
+  })
+
+  it('submits the form with valid values when button clicked', () => {
+    const submitFormData = jest.fn()
+    const fakeAPIUrl = 'https://fake.com/api/'
+    const fakeJSONBody = '{"foo": "bar"}'
+    const formMethod = 'POST'
+
+    const root = TestUtils.renderIntoDocument(
+      <Form submitFormData={submitFormData} />
+    )
+    const form = TestUtils.findRenderedDOMComponentWithTag(root, 'form')
+
+    const methodSelect = ReactDOM.findDOMNode(root).querySelector('select')
+
+    TestUtils.Simulate.change(methodSelect, { target: { value: formMethod } })
+
+    const urlInput = ReactDOM.findDOMNode(root).querySelector(
+      'input[name="url"]'
+    )
+    const bodyInput = ReactDOM.findDOMNode(root).querySelector('textarea')
+
+    TestUtils.Simulate.change(urlInput, { target: { value: fakeAPIUrl } })
+    TestUtils.Simulate.change(bodyInput, { target: { value: fakeJSONBody } })
+    TestUtils.Simulate.submit(form)
+
+    expect(submitFormData).toHaveBeenCalledWith({
+      body: fakeJSONBody,
+      method: formMethod,
+      url: fakeAPIUrl
+    })
+  })
+})
