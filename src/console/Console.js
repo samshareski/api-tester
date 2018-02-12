8c88562971d0103725ab3825a989b7af32b4ae6f
Sam Shareski <sam_shareski@otpp.com>
2018-02-12 08:12:35 -0500

    Add styles

--- src/console/Console.js
+++ src/console/Console.js
@@ -16,10 +16,12 @@ class Console extends Component {
   }
 
   render() {
-    return [
-      <Display response={this.state.response} />,
-      <Form submitFormData={this.testApi} />
-    ]
+    return (
+      <div className="two">
+        <Display response={this.state.response} />
+        <Form submitFormData={this.testApi} />
+      </div>
+    )
   }
 }
 
